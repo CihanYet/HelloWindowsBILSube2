3244891fec7c8905e3edb94a928bb0dc731d418b
Gazi <Gazi@DESKTOP-224SE1B>
2023-10-16 16:20:41 +0300

    Add project files.

---
+++ HelloWindowsBILSube2/frmEvent.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWindowsBILSube2
+{
+    public partial class frmEvent : Form
+    {
+        int sonuc = 0;
+        public frmEvent()
+        {
+            InitializeComponent();
+        }
+
+        private void frmEvent_MouseMove(object sender, MouseEventArgs e)
+        {
+            lblText.Text = "Gazi";
+            lblKoordinat.Text = e.Location.ToString();
+        }
+
+        private void frmEvent_MouseLeave(object sender, EventArgs e)
+        {
+            lblText.Text = String.Empty;
+        }
+
+        private void Topla(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            sonuc += int.Parse(btn.Text);
+           
+        }
+
+
+        private void btnSonuc_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"İşlemin sonucu:{sonuc}");
+        }
+    }
+}
